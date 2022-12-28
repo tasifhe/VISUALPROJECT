3495ce9d3f2de850821006d794cc6b915ceb71e3
Tasif Hossain Emon <tasifthe2002@gmail.com>
2022-12-28 17:16:51 +0600

    againChange

--- CPP_OOP/SMUCT_OOP/ArrayOP4.cpp
+++ CPP_OOP/SMUCT_OOP/ArrayOP4.cpp
@@ -1,20 +1,26 @@
 //Array Elemets addition
 #include<iostream>
+#include<stdlib.h>
 using namespace std;
 
 int main()
 {
     int n,a[101];
     int sum=0;
+    cout<<"Enter size of the array: ";
     cin>>n;
+    cout<<"Ente array elements: ";
     for(int i=0;i<n;i++)
     {
         cin>>a[i];
     }
+    cout<<"The elemetns are: ";
     for(int i=0;i<n;i++)
     {
         sum+=a[i];
     }
-    cout<<sum;
+    cout<<"Sum of the elements are: "<<sum<<endl;
+    cout<<"\nDONE"<<endl;
+    system("Color A0");
     return 0;
 }
