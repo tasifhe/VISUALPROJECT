275bd8371413b2e56913b14f98ce35aad902db99
Tasif Hossain Emon <tasifthe2002@gmail.com>
2022-12-28 17:12:25 +0600

    changeCommit2

--- CPP_OOP/SMUCT_OOP/ArrayOP3.cpp
+++ CPP_OOP/SMUCT_OOP/ArrayOP3.cpp
@@ -1,15 +1,19 @@
 //Print only odd values
 #include<iostream>
+#include<stdlib.h>
 using namespace std;
 
 int main()
 {
     int n,a[101];
+    cout<<"Enter size of the array: ";
     cin>>n;
+    cout<<"Ente array elements: ";
     for(int i=0;i<n;i++)
     {
         cin>>a[i];
     }
+    cout<<"The elemetns are: ";
     for(int i=0;i<n;i++)
     {
         if(a[i]&1)
@@ -17,5 +21,7 @@ int main()
             cout<<a[i]<<" ";
         }
     }
+    cout<<"\nDONE"<<endl;
+    system("Color A0");
     return 0;
 }
