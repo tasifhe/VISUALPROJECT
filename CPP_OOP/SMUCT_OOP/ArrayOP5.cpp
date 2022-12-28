769966017c998a41c9d4cc5c3a456f72f696e9ab
Tasif Hossain Emon <tasifthe2002@gmail.com>
2022-12-28 17:27:58 +0600

    pausechangecommit

--- CPP_OOP/SMUCT_OOP/ArrayOP5.cpp
+++ CPP_OOP/SMUCT_OOP/ArrayOP5.cpp
@@ -4,17 +4,24 @@ using namespace std;
 
 int main()
 {
-    int n,a[101];
-    int sum=0;
+    int a[101],n,temp;
+    cout<<"Enter the array size: ";
     cin>>n;
+    cout<<"Enter the array elements: ";
     for(int i=0;i<n;i++)
     {
         cin>>a[i];
     }
+    temp=a[0];
+    for(int i=1;i<=n-1;i++)
+    {
+        a[i-1]=a[i];
+    }
+    a[n-1]=temp;
+    cout<<"The modified array elements are: ";
     for(int i=0;i<n;i++)
     {
-        sum+=a[i];
+        cout<<a[i]<<" ";
     }
-    cout<<sum;
     return 0;
 }
